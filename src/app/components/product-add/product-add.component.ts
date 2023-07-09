7b9b7d5c9f803f7ac6bed22bd67fbd0b552b937c
FatihC <fatihgs1433@gmail.com>
2023-07-09 23:25:25 +0300

    Last 20

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,13 +1,13 @@
 import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'; // form oluşturmak için.
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 //FormGroup; 
 //FormBuilder;ReaciveForm'un servisi.(20. Ders) - Html ile controlleri ilişkilendirdiğimiz yer. Form oluşturmaya yarıyor.
 //FormControl;
-//Validators;   
+//Validators;   Doğrulama için
 
 
 @Component({
@@ -20,9 +20,7 @@ export class ProductAddComponent implements OnInit {
   productAddForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private productService: ProductService,
-    private toastrService: ToastrService) {
-
-  }
+    private toastrService: ToastrService) { }
   ngOnInit(): void {
     this.createProductAdd();
   }
@@ -37,26 +35,29 @@ export class ProductAddComponent implements OnInit {
   }
 
   addProduct() {
-
     if (this.productAddForm.valid) {
-      let productModel = Object.assign({}, this.productAddForm.value);//boş obje oluşturup creadeProductAdd'de yer alan productAddForm metodu içerisindeki prop.'ları atadık.
-      this.productService.add(productModel).subscribe(response=> {
+      let productModel = Object.assign({}, this.productAddForm.value); //boş obje oluşturup creadeProductAdd'de yer alan productAddForm metodu içerisindeki prop.'ları atadık.
 
-        this.toastrService.success(response.message,"Başarılı")
-      },responseError=>{console.log(responseError) 
-      this.toastrService.error(responseError.error)
+      this.productService.add(productModel).subscribe({
+        next: (response) => {
+          this.toastrService.success(response.message,"Başarılı");
+        },
+        error: (responseError) => {
+          if (responseError.error.Errors.length>0) {
+            console.log(responseError.error.Errors)
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+          
+              this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama Hatası");
+            }
+           
+          
+          }
+        }
       })
-       
-      
-
     }
     else {
-      this.toastrService.error("Formda boş alanlar var veya hatalı bir işlem yaptınız.", "Hata")
-
+      this.toastrService.error("Form hatalı girilmiş Lütfen kontrol ediniz.", "Dikkat");
     }
-
-
-
 
 
   }
