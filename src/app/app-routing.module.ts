2f369c9a5552ea8590ff08f76efc0f8339489130
ByFatiq <ByFatiq@DESKTOP-Q6GQDVV>
2023-07-25 00:42:07 +0300

    21. Ders 34. Dk. Login İşlemleri Gerçekleştirildi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+
+  //Layer gibi Sayfa Oluştururken Kullanıyoruz. Componenti çağırdığımız yer.
   // pathMatch:"full" eski versiyonlarda kullanıyoruz. Tam adres rota yapılanmasını sağlar.
-  {path:"",pathMatch:"full", component:ProductComponent},
-  {path:"products",component:ProductComponent},
-//path:"pathyolu:değişkenId"
-  {path:"products/category/:categoryId",component:ProductComponent},
-  {path:"products/add",component:ProductAddComponent}
+  { path: "", pathMatch: "full", component: ProductComponent },
+  { path: "products", component: ProductComponent },
+  //path:"pathyolu:değişkenId"
+  { path: "products/category/:categoryId", component: ProductComponent },
+  { path: "products/add", component: ProductAddComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
