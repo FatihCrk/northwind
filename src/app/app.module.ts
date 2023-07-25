c6015c1a3dcb2a68765bdeb34097323b31d5468c
ByFatiq <ByFatiq@DESKTOP-Q6GQDVV>
2023-07-26 00:25:07 +0300

    21. Ders son 20. Dk.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,17 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -35,7 +36,7 @@ import { LoginComponent } from './components/login/login.component';
     FilterPipePipe,
     ProductAddComponent,
     LoginComponent,
-    
+
   ],
   imports: [
     BrowserModule,
@@ -47,13 +48,13 @@ import { LoginComponent } from './components/login/login.component';
     ReactiveFormsModule,
 
     // Animasyonlu Toastr için ekledik.
-    ToastrModule.forRoot({positionClass:"toast-bottom-right"}),
+    ToastrModule.forRoot({ positionClass: "toast-bottom-right" }),
     BrowserAnimationsModule
-   
+
   ],
   providers: [
-  
-    
+    { provide: HTTP_INTERCEPTORS,useClass : AuthInterceptor,multi:true } //Tüm servisler için Enjekte ediyoruz. 
+    //AuthInterceptor ve çoklu kullanımı aktif ediyoruz.
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+//mmiddleware'lar gibi araya girecek intercepterlar yazılabilir. Hata kodu verdiğinde ne yapayım gibi...
+
+//interceptor araya girmek- pakete Token ekleyip göndermek için.
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = localStorage.getItem("token");
+    let newRequest: HttpRequest<any>;
+    newRequest = request.clone({   //Request Klonu oluşturduk. Headers, Bearer ve Token alanı Oluşturuyoruz ve Set ediyoruz..
+      headers: request.headers.set("Authorization", "Bearer" + token)
+    })
+    return next.handle(newRequest);   //Token ile handle ediyoruz.
+  }
+}
