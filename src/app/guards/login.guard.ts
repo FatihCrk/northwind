3a8ff4446ac4f54a342d7f4f22fad1769ded938a
Fatih ÇIRAK <fatihcirak@halic.edu.tr>
2023-07-27 16:06:52 +0300

    Tüm Dersler Sonu

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {  Router,  UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
@@ -9,17 +9,16 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginGuard  {
   constructor(
-    // private authService: AuthService,
-    // private toastrService: ToastrService,
-    // private router: Router
-    const canActivateTeam: CanActivateFn =(route:ActivatedRouteSnapshot, state:RouterStateSnapshot ) ) { }
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+    ) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-
-
+    canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     if (this.authService.isAuthenticated()) {
       return true;
     }
